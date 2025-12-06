3b6c8447f70e67a1b204cedf40381d09b4bc4180
arua2002 <tagirovr277@gmail.com>
2025-12-06 21:07:13 +0500

    karimu

--- sudoku.ino
+++ sudoku.ino
@@ -1,20 +1,24 @@
 #include "SudokuSolver.h"
-#include "Sensor.h"
-#include <Dynamixel2Arduino.h>
+#include "CameraModule.h"
+#include <TrackingCamI2C.h>
 
+TrackingCamI2C trackingCam;
 SudokuSolver solver;
-Sensor mySensor;
+CameraModule myCAM;
+
 const int N = 3;
-int initia[N][N] = {
-    {1, 0, 0},
-    {0, 2, 0},
-    {0, 0, 3}
+bool search = false;
+uint8_t n = 0;
+
+int initial[N][N] = {
+    {0, 0, 0},
+    {0, 0, 0},
+    {0, 0, 0}
 };
 
 // Первая задача (обычная в основном ядре)
-void sud(int initial[N][N]){
+void sud(int initial[N][N]) {
   solver.setInitial(initial);
-
   if (solver.solve()) {
     int solution[N][N];
     solver.getBoard(solution);
@@ -30,31 +34,38 @@ void sud(int initial[N][N]){
     Serial.println("Нет решения.");
   }
 }
+
+// Функция задачи для второго ядра
+void core2(void *pvParameters) {
+
+  trackingCam.init(51, 400000);
+  delay(5000); 
+
+  while (true) {
+    if (search) {
+      n = trackingCam.readBlobs(3); // чтение максимум 3 объектов
+    }
+    delay(1000); // задержка между чтениями
+  }
+}
 void setup() {
   Serial.begin(9600);
+  sud(initial);
 
-  sud(initia);
-
-  // Создаем задачу на втором ядре
   #if defined(ESP32)
+    // Создаем задачу на втором ядре
     xTaskCreatePinnedToCore(
-      core2,          // функция задачи
-      "Core2Task",    // имя задачи
-      10000,          // размер стека
-      NULL,           // параметры
-      1,              // приоритет
-      NULL,           // указатель на задачу
-      1               // номер ядра (1 или 0)
+      core2,           // функция задачи
+      "Core2Task",     // имя задачи
+      10000,           // размер стека
+      NULL,            // параметры
+      1,               // приоритет
+      NULL,            // указатель на задачу
+      1                // ядро (1 или 0)
     );
   #endif
 }
 
 void loop() {
-  // пусто
-}
-void core2() {
-  while (true) {
-    // Пустое ядро, ничего не делает
-    delay(1000);
-  }
+  //добавь здесь код где он ездит и когда останавливается searh делай true и не большая задержка и присваивай N[такой то,такой]=n
 }
